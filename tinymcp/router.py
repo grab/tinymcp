e0496cee00fcf42a9e2361128eed37c734bfacc4
Riyadh Sharif <riyadh.sharif@grabtaxi.com>
2025-08-28 18:03:21 +0800

    add cr notice

--- tinymcp/router.py
+++ tinymcp/router.py
@@ -1,3 +1,8 @@
+"""
+Copyright 2025 Grabtaxi Holdings Pte Ltd (GRAB), All rights reserved.
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
+"""
+
 import json
 from typing import Any, Dict
 
